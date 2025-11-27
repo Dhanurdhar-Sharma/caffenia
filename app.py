f733e6efe6e9e2547aa2978a07196ba1b1323904
Dhanurdhar Sharma <dhanurdhar1724@gmail.com>
2025-11-27 07:52:52 +0530

    edit requirement.txt

--- app.py
+++ app.py
@@ -203,6 +203,6 @@ def verify():
 # 🏁 RUN SERVER
 # ---------------------------------------------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5003))
     logging.info(f"🚀 Starting server on port {port}")
     app.run(host="0.0.0.0", port=port)
